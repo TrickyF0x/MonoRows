d9a4eebe2861bea734a0f2210cb4a4d8dde50c1a
Semenkov Alexey <sealekssaa@gmail.com>
2019-02-03 12:35:38 +0700

    alpha

---
+++ array_with_mono_rows/monorows.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+
+class MonoRows
+{
+public:
+	MonoRows();
+
+	std::vector<std::vector<int>> matrix; //массив n*n
+	std::vector<std::vector<int>> MonotonousArray()
+	{
+
+	}
+	~MonoRows();
+
+private:
+
+};
+
+MonoRows::MonoRows()
+{
+};
+
+MonoRows::~MonoRows()
+{
+};
